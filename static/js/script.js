a24fda9ebfa1aa502d53b23759587c08a87321bb
Miunn <remcaulier@orange.fr>
2023-05-10 12:16:54 +0200

    Add external links and resize assets

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,8 @@
+// -------- Observer for skills container -------- //
+
 var quantities = ["80%", "80%", "70%", "60%", "70%", "60%"]
 
-function observerCallback(entries, observer) {
+function skillsObserverCallback(entries, observer) {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             let elmt = entry.target;
@@ -33,20 +35,35 @@ function observerCallback(entries, observer) {
                     break;
             }
         }
-    })
+    });
 }
 
 let options = {
     root: null, // Default root -> browser viewport
     rootMargin: "0px",
-    threshold: 0.5  // Invoke callback when 100% of target is visible
+    threshold: 0.5  // Invoke callback when 50% of target is visible
 }
 
-let observer = new IntersectionObserver(observerCallback, options);
+let observer = new IntersectionObserver(skillsObserverCallback, options);
 
 let targets = document.getElementsByClassName("progress-value");
-console.log(targets.length);
 for (i = 0; i < targets.length; i++) {
     console.log(targets.item(i));
     observer.observe(targets.item(i));
 }
+
+// -------- Listener for links container -------- //
+
+const elScrollable = document.querySelector("html")
+const elNav = document.querySelector("#links-container");
+const threshold = window.innerHeight - 75 - 0.05*window.innerHeight;
+console.log(window.innerHeight, threshold);
+const handleNav = () => {
+  const scrollTop = elScrollable.scrollTop;
+  console.log(scrollTop);
+  elNav.classList.toggle("top-fixed", scrollTop > threshold);
+};
+
+addEventListener("scroll", handleNav)
+addEventListener("resize", handleNav)
+handleNav();
